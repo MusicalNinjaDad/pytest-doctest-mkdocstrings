5b0c5f74f8c72056faf2dbd0aa8abe413770cc1d
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-01 19:45:24 +0100

    improved readability

--- pytest_doctest_mkdocstrings/patch_doctest.py
+++ pytest_doctest_mkdocstrings/patch_doctest.py
@@ -1,17 +1,20 @@
 # noqa: D100
-
-def pytest_sessionstart(session) -> None:  # noqa: ANN001, ARG001
+import pytest
+def pytest_sessionstart(session: pytest.Session) -> None:
     """
     Monkeypatches DocTests Regex to ignore end of codeblock marker.
 
     Run by pytest before beginning collection
     """
+    del session  # We don't need the session info
+
     import doctest
     import re
 
-# https://github.com/python/cpython/blob/a0a8d9ffe0ddb0f55aeb02801f48e722c2660ed3/Lib/doctest.py#L613
+    # https://github.com/python/cpython/blob/a0a8d9ffe0ddb0f55aeb02801f48e722c2660ed3/Lib/doctest.py#L613
 
-    _ORIGINAL_EXAMPLE_RE = re.compile(r"""
+    _ORIGINAL_EXAMPLE_RE = re.compile(  # noqa: N806
+        r"""
         # Source consists of a PS1 line followed by zero or more PS2 lines.
         (?P<source>
             (?:^(?P<indent> [ ]*) >>>    .*)    # PS1 line
@@ -22,9 +25,12 @@ def pytest_sessionstart(session) -> None:  # noqa: ANN001, ARG001
                      (?![ ]*>>>)  # Not a line starting with PS1
                      .+$\n?       # But any other line
                   )*)
-        """, re.MULTILINE | re.VERBOSE)  # noqa: N806
+        """,
+        re.MULTILINE | re.VERBOSE,
+    )
 
-    _MD_EXAMPLE_RE = re.compile(r"""
+    _MD_EXAMPLE_RE = re.compile(  # noqa: N806
+        r"""
             # Source consists of a PS1 line followed by zero or more PS2 lines.
             (?P<source>
                 (?:^(?P<indent> [ ]*) >>>    .*)    # PS1 line
@@ -36,7 +42,9 @@ def pytest_sessionstart(session) -> None:  # noqa: ANN001, ARG001
                         (?![ ]*>>>)  # Not a line starting with PS1
                         .+$\n?       # But any other line
                     )*)
-            """, re.MULTILINE | re.VERBOSE)  # noqa: N806
+            """,
+        re.MULTILINE | re.VERBOSE,
+    )
 
     p = doctest.DocTestParser
     if p._EXAMPLE_RE == _ORIGINAL_EXAMPLE_RE:  # noqa: SLF001
