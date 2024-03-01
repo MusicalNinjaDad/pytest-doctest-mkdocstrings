df46377c1a82678262aaef7a343ce07edc122926
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-01 21:37:25 +0100

    test the patch via pytester

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -12,3 +12,8 @@ def test_pytester(pytester):
     result = pytester.runpytest()
 
     result.assert_outcomes(failed=1, passed=1)
+
+def test_patch(pytester):
+    pytester.copy_example("test_patch.py")
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)
