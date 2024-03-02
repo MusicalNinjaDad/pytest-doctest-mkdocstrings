d03b6f9886d0ef21d1df07c8f5460dc3947f9d37
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-02 08:35:08 +0100

    RED: add iniconfig option

--- test/conftest.py
+++ test/conftest.py
@@ -1,1 +1,13 @@
+import pytest
+
 pytest_plugins = ["pytester"]
+
+
+@pytest.fixture
+def _codeblocksini(pytester):
+    inicontents = """
+    [pytest]
+    doctest-mdcodeblocks = true
+    """
+
+    pytester.makeini(inicontents)
--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_pytester(pytester):
     pytester.makepyfile(
         """
@@ -6,7 +9,7 @@ def test_pytester(pytester):
             
         def test_fail():
             assert False
-        """
+        """  # noqa: COM812
     )
 
     result = pytester.runpytest()
@@ -22,3 +25,9 @@ def test_nopatch(pytester):
     pytester.copy_example("test_patch.py")
     result = pytester.runpytest()
     result.assert_outcomes(failed=1)
+
+@pytest.mark.usefixtures("_codeblocksini")
+def test_inioption(pytester):
+    pytester.copy_example("test_patch.py")
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)
