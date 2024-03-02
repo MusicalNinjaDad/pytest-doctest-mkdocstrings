7e961b7690cdc401d6980629b91d7e1007721257
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-02 08:46:39 +0100

    remove test that only checks pytester is working

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -1,21 +1,6 @@
 import pytest
 
 
-def test_pytester(pytester):
-    pytester.makepyfile(
-        """
-        def test_pass():
-            assert True
-            
-        def test_fail():
-            assert False
-        """  # noqa: COM812
-    )
-
-    result = pytester.runpytest()
-
-    result.assert_outcomes(failed=1, passed=1)
-
 def test_patch(pytester):
     pytester.copy_example("test_patch.py")
     result = pytester.runpytest("--doctest-mdcodeblocks")
