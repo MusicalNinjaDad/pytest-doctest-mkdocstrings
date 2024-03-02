d3bbf417f596d672b0dfb1ad86250b5565422cc0
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-02 08:41:11 +0100

    RED: override with no-doctest-mdcodeblocks

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -31,3 +31,10 @@ def test_inioption(pytester):
     pytester.copy_example("test_patch.py")
     result = pytester.runpytest()
     result.assert_outcomes(passed=1)
+
+
+@pytest.mark.usefixtures("_codeblocksini")
+def test_override_inioption(pytester):
+    pytester.copy_example("test_patch.py")
+    result = pytester.runpytest("--no-doctest-mdcodeblocks")
+    result.assert_outcomes(failed=1)
