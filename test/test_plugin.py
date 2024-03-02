ce3f9317a698ba1bdcc1aba0796b99834dc70324
Mike Foster <i.am.mike.foster@gmail.com>
2024-03-02 10:15:13 +0100

    GREEN: update expected test reults

--- test/test_plugin.py
+++ test/test_plugin.py
@@ -17,9 +17,9 @@ def test_inioption_set(pytester, runoptions, results):
 @pytest.mark.usefixtures("_nosettingini")
 @pytest.mark.parametrize(("runoptions", "results"),
     [
-        pytest.param("", {"failed" : 2, "passed": 1},id="Default"),
-        pytest.param("--doctest-mdcodeblocks", {"passed": 3},id="Enabled"),
-        pytest.param("--no-doctest-mdcodeblocks", {"failed" : 2, "passed": 1}, id="Disabled"),
+        pytest.param("", {"failed" : 3, "passed": 1},id="Default"),
+        pytest.param("--doctest-mdcodeblocks", {"passed": 4},id="Enabled"),
+        pytest.param("--no-doctest-mdcodeblocks", {"failed" : 3, "passed": 1}, id="Disabled"),
     ],
 )
 def test_noini(pytester, runoptions, results):
@@ -29,9 +29,9 @@ def test_noini(pytester, runoptions, results):
 @pytest.mark.usefixtures("_disabledcodeblocksini")
 @pytest.mark.parametrize(("runoptions", "results"),
     [
-        pytest.param("", {"failed": 2, "passed" : 1},id="No override"),
-        pytest.param("--doctest-mdcodeblocks", {"passed": 3},id="Override"),
-        pytest.param("--no-doctest-mdcodeblocks", {"failed": 2, "passed" : 1}, id="Duplication"),
+        pytest.param("", {"failed": 3, "passed" : 1},id="No override"),
+        pytest.param("--doctest-mdcodeblocks", {"passed": 4},id="Override"),
+        pytest.param("--no-doctest-mdcodeblocks", {"failed": 3, "passed" : 1}, id="Duplication"),
     ],
 )
 def test_inioption_disabled(pytester, runoptions, results):
